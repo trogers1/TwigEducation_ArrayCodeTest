fa1635e7804fb238615eec49ea81a44ffdf4f2eb
Taylor Rogers <taylor.rogers71@gmail.com>
2020-10-29 10:00:19 +0000

    Fix first test

--- index.test.js
+++ index.test.js
@@ -1,10 +1,11 @@
 const groupArrayItems = require('./index');
-const assert = require('assert')
+const assert = require('assert');
 
 describe('groupArrayItems', () => {
-    it('should throw an error for invalid input', () => {
-        it('should throw an error if N arg is negative', () => {
-            assert.throws(() => groupArrayItems([1, 2, 3, 4], -1))
-        })
+  describe('=> should throw an error for invalid input', () => {
+    it('=> should throw an error if N arg is negative', () => {
+      groupArrayItems([1, 2, 3, 4], -1);
+      assert.throws(() => groupArrayItems([1, 2, 3, 4], -1));
     });
   });
+});
