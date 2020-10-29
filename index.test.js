282ee997eb81f5296c0b5671368da56d2aa5e0ef
Taylor Rogers <taylor.rogers71@gmail.com>
2020-10-29 10:06:44 +0000

    Add non-integer test

--- index.test.js
+++ index.test.js
@@ -2,9 +2,15 @@ const groupArrayItems = require('./index');
 const assert = require('assert');
 
 describe('groupArrayItems', () => {
-  describe('=> should throw an error for invalid input', () => {
+  describe('should throw an error for invalid input', () => {
     it('=> should throw an error if N arg is negative', () => {
       assert.throws(() => groupArrayItems([1, 2, 3, 4], -1));
+    });
+    it('=> should throw an error if N arg is not an integer', () => {
+      assert.throws(() => groupArrayItems([1, 2, 3, 4], 3.4));
+      assert.throws(() => groupArrayItems([1, 2, 3, 4], '4'));
+      assert.throws(() => groupArrayItems([1, 2, 3, 4], 'test'));
+      assert.throws(() => groupArrayItems([1, 2, 3, 4], true));
     });
   });
 });
